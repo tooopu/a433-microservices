0a323df8c76eaa263f4efca6adacf8e2b646ce46
tooopu <topumum@gmail.com>
2023-03-16 12:14:26 +0700

    add script and ci conf

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#Perintah untuk build Docker image dari berkas Dockerfile yang disediakan dengan nama <username-docker>/karsajobs-ui:latest
+docker build -t ghcr.io/tooopu/a433-microservices/karsajobs-ui:latest .
+
+#We recommend saving your token as an environment variable. Using format ghp_ for Personal Access Tokens and export to CR_PAT then using it to login ke github packages
+echo $CR_PAT | docker login ghcr.io --username tooopu --password-stdin
+
+#Mengunggah image ke Github Packages
+docker push ghcr.io/tooopu/a433-microservices/karsajobs-ui:latest
